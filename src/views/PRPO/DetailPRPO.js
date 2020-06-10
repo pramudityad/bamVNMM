b8c79e5aeee88f2d9e58c8b6312c5a0a48e3d360
Damar Pramuditya <damar.pramuditya@ericsson.com>
2020-06-10 16:12:14 +0700

    edit prt

--- src/views/PRPO/DetailPRPO.js
+++ src/views/PRPO/DetailPRPO.js
@@ -55,21 +55,21 @@ class DetailPRPO extends Component {
     const wb = new Excel.Workbook();
     const ws = wb.addWorksheet();
 
-    const data_assingment = this.state.all_data;
+    const data_prt = this.state.all_data;
     let indexSSOW = 7;
 
-    let headerRow = ["prt_id", "site_id", "site_name", "quotation_number", "signum_pm", "approval_by", "id_project_doc", "project_name", "area", "purchase_group","asp_name","term_of_payment","network_number","activity_code","action_point","currency","current_status","approval_status","approval_date","total_price","pr_number","pr_date", "pr_inserted_by", "po_number","po_date","po_inserted_by","po_item","bast_no_dp","req_gr_dp","req_gr_by_dp","req_gr_date_dp","req_revision_dp","revision_done_dp", "bast_no_final", "req_gr_final", "req_gr_by_final", "req_gr_date_final","req_revision_final","revision_done_final", "SSOW_List"];
+    let headerRow = ["prt_id", "site_id", "site_name", "quotation_number", "signum_pm", "approval_by", "project_name", "area", "purchase_group","asp_name","term_of_payment","network_number","activity_code","action_point","currency","current_status","approval_status","approval_date","total_price","pr_number","pr_date", "pr_inserted_by", "po_number","po_date","po_inserted_by","po_item","bast_no_dp","req_gr_dp","req_gr_by_dp","req_gr_date_dp","req_revision_dp","revision_done_dp", "bast_no_final", "req_gr_final", "req_gr_by_final", "req_gr_date_final","req_revision_final","revision_done_final", "SSOW_List"];
 
-    // data_assingment.SSOW_List.map((e, idx) => headerRow.push("ssow_" + (e.sow_type.toLowerCase()) + "_id_" + (idx + 1).toString(), "ssow_" + (e.sow_type.toLowerCase()) + "_activity_number_" + (idx + 1).toString(), "ssow_" + (e.sow_type.toLowerCase()) + "_unit_" + (idx + 1).toString(), "ssow_" + (e.sow_type.toLowerCase()) + "_quantity_" + (idx + 1).toString()));
+    // data_prt.SSOW_List.map((e, idx) => headerRow.push("ssow_" + (e.sow_type.toLowerCase()) + "_id_" + (idx + 1).toString(), "ssow_" + (e.sow_type.toLowerCase()) + "_activity_number_" + (idx + 1).toString(), "ssow_" + (e.sow_type.toLowerCase()) + "_unit_" + (idx + 1).toString(), "ssow_" + (e.sow_type.toLowerCase()) + "_quantity_" + (idx + 1).toString()));
 
-    // let dataPRT = [data_assingment.Assignment_No, data_assingment.Project, data_assingment.SOW_Type, "cd_id", data_assingment.Vendor_Code_Number, data_assingment.Vendor_Name, this.convertTOP(data_assingment.Payment_Terms), data_assingment.Site_ID];
-    // data_assingment.SSOW_List.map(e => dataPRT.push(e.ssow_id, e.ssow_activity_number, e.ssow_unit, e.ssow_qty));
+    let dataPRT = [data_prt.prt_id, data_prt.site_id, data_prt.site_name, data_prt.quotation_number, data_prt.signum_pm, data_prt.approval_by, data_prt.project_name, data_prt.area, data_prt.purchase_group, data_prt.asp_name, data_prt.term_of_payment, data_prt.network_number, data_prt.activity_code, data_prt.action_point, data_prt.currency, data_prt.approval_status, data_prt.approval_date, data_prt.total_price, data_prt.pr_number, data_prt.pr_date, data_prt.pr_inserted_by, data_prt.po_number, data_prt.po_date, data_prt.po_inserted_by, data_prt.po_item, data_prt.bast_no_dp, data_prt.req_gr_dp, data_prt.req_gr_by_dp, data_prt.req_gr_date_dp, data_prt.req_revision_dp, data_prt.revision_done_dp, data_prt.bast_no_final, data_prt.req_gr_final, data_prt.req_gr_by_final, data_prt.req_gr_date_final, data_prt.req_revision_final, data_prt.revision_done_final];
+    // data_prt.SSOW_List.map(e => dataPRT.push(e.ssow_id, e.ssow_activity_number, e.ssow_unit, e.ssow_qty));
 
     ws.addRow(headerRow);
-    // ws.addRow(dataPRT);
+    ws.addRow(dataPRT);
 
     const MRFormat = await wb.xlsx.writeBuffer();
-    saveAs(new Blob([MRFormat]), 'Assignment ' + data_assingment.prt_id + ' Template.xlsx');
+    saveAs(new Blob([MRFormat]), 'Assignment ' + data_prt.prt_id + ' Template.xlsx');
   }
 
   render() {
@@ -95,7 +95,7 @@ class DetailPRPO extends Component {
                   ></i>
                   PRT Detail ({all_data.prt_id})
                 </span>
-                <Link to={'/prt-edit/' + all_data._id}>
+                <Link to={'/prt-list/edit/' + all_data._id}>
                 <Button
                   style={{ marginRight: "8px", float: "right" }}                  
                   color="warning"
