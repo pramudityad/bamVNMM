a6499456301ac030a5d1f91af1db0fb7b6235da0
rochateer <aroccha27@gmail.com>
2020-06-12 12:59:21 +0700

    push

--- src/views/MR/WHManagement/WHManagement.js
+++ src/views/MR/WHManagement/WHManagement.js
@@ -535,7 +535,15 @@ class WHManagement extends React.Component {
       });
     } else {
       this.toggleLoading();
-      this.setState({ action_status: "failed" });
+      if (patchData.response !== undefined && patchData.response.data !== undefined && patchData.response.data.error !== undefined) {
+        if (patchData.response.data.error.message !== undefined) {
+          this.setState({ action_status: 'failed', action_message: patchData.response.data.error.message });
+        } else {
+          this.setState({ action_status: 'failed', action_message: patchData.response.data.error });
+        }
+      } else {
+        this.setState({ action_status: 'failed' });
+      }
     }
   }
 
@@ -569,9 +577,15 @@ class WHManagement extends React.Component {
           this.toggleLoading();
         });
       } else {
-        this.setState({
-          action_status: "failed",
-        });
+        if (res.response !== undefined && res.response.data !== undefined && res.response.data.error !== undefined) {
+          if (res.response.data.error.message !== undefined) {
+            this.setState({ action_status: 'failed', action_message: res.response.data.error.message });
+          } else {
+            this.setState({ action_status: 'failed', action_message: res.response.data.error });
+          }
+        } else {
+          this.setState({ action_status: 'failed' });
+        }
         this.toggleLoading();
       }
     });
@@ -655,10 +669,10 @@ class WHManagement extends React.Component {
   };
 
   exportMatStatus = async () => {
-    
+
     const wb = new Excel.Workbook();
     const ws = wb.addWorksheet();
-    const ws2 = wb.addWorksheet();    
+    const ws2 = wb.addWorksheet();
     const aspData = this.state.asp_data;
     // console.log('aspData ', aspData);
 
@@ -682,7 +696,7 @@ class WHManagement extends React.Component {
       // const element = aspData[i];
       ws2.addRow([aspData[i].Name, aspData[i].Vendor_Code]);
     }
-    
+
 
     const PPFormat = await wb.xlsx.writeBuffer();
     saveAs(new Blob([PPFormat]), "WH Management Template.xlsx");
