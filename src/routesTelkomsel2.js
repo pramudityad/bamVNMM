495999a3bef86c168410f23254d470a23b8b11b5
pramudityad <pramudityad@yahoo.com>
2021-08-13 11:33:21 +0700

    change routes to telkomsel

--- src/routesTelkomsel2.js
+++ src/routesTelkomsel2.js
@@ -52,9 +52,9 @@ const PSBOMDetail = React.lazy(() => import("./viewsTelkomsel2/PS/DetailPS"));
 
 const CRDetail = React.lazy(() => import("./viewsTelkomsel2/CR/CRDetail"));
 
-const MRList = React.lazy(() => import("./viewsTelkomsel2/MR/MRList"));
+const MRList = React.lazy(() => import("./viewsTelkomsel/MR/MRList"));
 const MRNAList = React.lazy(() => import("./viewsTelkomsel2/MR/MRNAList"));
-const MRCreation = React.lazy(() => import("./viewsTelkomsel2/MR/MRCreation2"));
+const MRCreation = React.lazy(() => import("./viewsTelkomsel/MR/MRCreation2"));
 const BulkMRCreation = React.lazy(() => import("./viewsTelkomsel2/MR/BulkMR"));
 const MRDetail = React.lazy(() => import("./viewsTelkomsel2/MR/MRDetail"));
 const PSUpload = React.lazy(() => import("./viewsTelkomsel2/MR/PSUpload"));
